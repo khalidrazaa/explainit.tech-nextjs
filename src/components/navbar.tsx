b2c195bc63c01b02b1ec1b19bf35375da7b6a4c8
khalidrazaa <khalid_raza@live.com>
2025-06-23 09:57:59 +0530

    added home page & nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,23 +4,33 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 export default function Navbar() {
-  const [search, setSearch] = useState('');
+  const [searchText, setSearchText] = useState('');
 
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-white shadow-md">
-      <div className="flex gap-6 text-lg font-medium">
-        <Link href="/">Home</Link>
-        <Link href="/articles">Articles</Link>
-        <Link href="/about">About Us</Link>
+      
+      {/* Logo on left */}
+      <div className="text-xl font-bold text-blue">
+        <Link href="/">
+          <h1 className="text-xl font-bold text-black">explainit.tech</h1>
+        </Link>
       </div>
 
-      <div>
+      {/* Center Nav Buttons */}
+      <div className="space-x-6 text-lg font-medium hidden md:flex">
+        <Link href="/" className="hover:text-black">Home</Link>
+        <Link href="/articles" className="hover:text-blue-600">Articles</Link>
+        <Link href="/about" className="hover:text-blue-600">About Us</Link>
+      </div>
+
+      {/* Search Bar on Right*/}
+      <div className="flex items-center">
         <input
           type="text"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-          placeholder="Search..."
-          className="px-3 py-1 border rounded-md focus:outline-none focus:ring"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          placeholder="Search Articles..."
+          className="px-3 py-1 border rounded-md focus:outline-none focus:ring text-sm"
         />
       </div>
     </nav>

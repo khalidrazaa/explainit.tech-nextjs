67f3f9479a2245956feb6ecd1dca0fe6e94d9db1
khalidrazaa <khalid_raza@live.com>
2026-02-28 15:48:48 +0530

    just save everything

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,31 @@
-import "./globals.css";
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Demystifying Tech with Explainit.Tech",
+};
+
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+        </head>
+        <body>
+          <Navbar/>
+          <main className="max--w-6xl mx-auto px-4 py-6">
+          {children}
+          </main>
+        </body>
+      </html>
+    </>
   );
 }
